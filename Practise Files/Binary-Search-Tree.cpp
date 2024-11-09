5c842b78cdb7434aed61f969494637fa9d2c89a1
Mushtasin Ahmed <74264555+Mafia-1911@users.noreply.github.com>
2024-11-09 21:12:07 +0600

    Update Binary-Search-Tree.cpp

--- Practise Files/Binary-Search-Tree.cpp
+++ Practise Files/Binary-Search-Tree.cpp
@@ -1,1 +1,6 @@
-#include <iostream>
+#include<iostream>
+using namespace std;
+//The right side of the root node contains values greater
+//The left side contains less than values
+// O(log n) to find and insert and - its very efficient
+// The number of level the node is , the number of steps we need to take
