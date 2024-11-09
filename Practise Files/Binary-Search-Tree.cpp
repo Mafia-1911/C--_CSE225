5c47cf6282f314b8fc6da04f8798a32306177858
Mushtasin Ahmed <74264555+Mafia-1911@users.noreply.github.com>
2024-11-09 21:13:01 +0600

    Update Binary-Search-Tree.cpp

--- Practise Files/Binary-Search-Tree.cpp
+++ Practise Files/Binary-Search-Tree.cpp
@@ -36,3 +36,34 @@ public:
 //                                The Right side
 //            if nullptr insert at that position
 //            else move to the next node
+  bool insert(int value){
+        Node* newNode= new Node(value);
+        if(root==nullptr){
+            root=newNode;
+            return true;
+        }
+        Node* temp=root;
+        while(true){
+            if(temp->value == newNode->value ){
+                //Duplicate Value Found
+                cout<<"Value already Exists";
+                return false;
+            }else if(newNode->value > temp->value ){
+                //Move right
+                if(temp->right == nullptr){
+                    temp->right=newNode;
+                    return true;
+                }else {
+                    temp=temp->right;
+                }
+            }else if(newNode->value < temp->value){
+                //Move left
+                if(temp->left==nullptr){
+                    temp->left=newNode;
+                    return true;
+                }else{
+                    temp=temp->left;
+                }
+            }
+        }
+    }
