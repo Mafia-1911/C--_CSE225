7cb1c4bb0c76cbc2ed36f68f792a6e3abd38fa51
Mushtasin Ahmed <74264555+Mafia-1911@users.noreply.github.com>
2024-12-04 23:57:54 +0600

    Update Lab-Final.cpp

--- Lab-Final.cpp
+++ Lab-Final.cpp
@@ -1,1 +1,15 @@
+    bool found50=false;//Setting it initially to be not found
+    bst.search(50,found50);
+    if(found50){
+        std::cout<<"Search 50: found"<<endl;
+    }else{
+        std::cout<<"Search 50: not found"<<endl;
+    }
 
+    int height=bst.height();
+    cout<<"Height of the tree: "<<height<<endl;
+    
+
+
+return 0;
+}
